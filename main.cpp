d458619cba2115107a28b8d48ebec015b7e5a8a3
jinsu <dnqlthsus9@gmail.com>
2024-09-08 15:20:11 +0900

    2강 자료형

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    // 2강 자료형 (byte)
+    /**
+     * bit : 메모리 최소 단위 , 0 또는 1
+     * 1 byte = 8 bit
+     * 
+     * 1byte : 256(2^8) 개의 경우의 수 를 표현 할 수 있다.
+     * 
+     * 양의 정수 : 0 ~ 255 까지 표현 가능
+     * 
+     * 1024 (2^10) byte = 1 KB
+     * 1024 KB = 1 MB
+     * 1024 MB = 1 GB
+     * 1024 GB = 1 TB
+     * 
+     * 
+     */
+    /**
+     * 정수형 : char(1) , short(2), int(4) , long(4), 
+     * 실수형 : float(4), double(8)
+     */
+    int i = 0;
+
+
+    // 양의 정수만 표현 한다는 의미 : unsigned
+    unsigned char j = 0;
+
+    /**
+     * = : 대입연산자
+     * == , != , < , > : 비교연산자
+     */
+
+
+
+    // 3강 정수형
+
+    cout << "hello";
+    return 0;
+}
